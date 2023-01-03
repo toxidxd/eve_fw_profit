79283e2e8fc4ff1ab065e19e9a1c1c23e28d88d0
toxidxd <toxid13@yandex.ru>
2023-01-04 00:20:35 +0300

    full rework

--- price_request.py
+++ price_request.py
@@ -3,50 +3,75 @@ import json
 import requests
 from fake_useragent import UserAgent
 
-data = {"market_name": "amarr",
-        "items": [{"name": "Tritanium"},
-                  {"name": "Pyerite"},
-                  {"name": "Mexallon"},
-                  {"name": "Isogen"},
-                  {"name": "Nocxium"},
-                  {"name": "Zydrine"},
-                  {"name": "Megacyte"},
-                  {"name": "Auto-Integrity Preservation Seal"},
-                  {"name": "Life Support Backup Unit"},
-                  {"name": "Coercer Navy Issue"},
-                  {"name": "Omen Navy Issue"},
-                  {"name": "Augoror Navy Issue"},
-                  {"name": "Prophecy Navy Issue"}]
-        }
 
-jdata = requests.post("https://evepraisal.com/appraisal/structured.json",
-                      data=json.dumps(data),
-                      headers={'User-Agent': UserAgent().chrome}).text
+def data_reader(csv_file):
+    csv_data = []
+    with open(csv_file) as csv_f:
+        reader = csv.reader(csv_f, delimiter=',')
+        for i_line in reader:
+            csv_data.append(i_line)
 
-data = json.loads(jdata)
-print(type(data))
-all_mats = []
+    return csv_data  # возвращает список списков из cvs файла
 
-for material in data["appraisal"]["items"]:
-    print(material["name"], " sell", material["prices"]["sell"]["percentile"])
-    print(material["name"], " buy", material["prices"]["buy"]["percentile"])
 
-    cur_mat = [material["name"],
-               str(material["prices"]["sell"]["percentile"]).replace('.', ','),
-               str(material["prices"]["buy"]["percentile"]).replace('.', ',')]
+def price_parser(data, hub):
+    jdata = requests.post("https://evepraisal.com/appraisal/structured.json",
+                          data=json.dumps(data_to_request(data, hub)),
+                          headers={'User-Agent': UserAgent().chrome}).text
 
-    all_mats.append(cur_mat)
+    data = json.loads(jdata)
+    all_prices = dict()
 
-with open("materials_price.csv", "w", newline='') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(["mat", "sell", "buy"])
-    for line in all_mats:
-        writer.writerow(line)
+    for material in data["appraisal"]["items"]:
+        all_prices[material["name"]] = {'sell': str(material["prices"]["sell"]["min"]),
+                                        'buy': str(material["prices"]["buy"]["max"])}
+    return all_prices
 
-data = []
-with open("data.csv", "r",newline='') as csv_f:
-    for x in csv_f:
-        data.append(x.split(','))
 
-print(data)
-print("File wrote!")
+def data_to_request(data, hub):
+    return {"market_name": hub,
+            "items": [{'name': i_item} for i_item in data[0][5:]] +
+                     [{'name': i_item[0]} for i_item in data[1:]]}
+
+
+def profit_calc(items_data, prices):
+    result = []
+    for i_item in items_data[1:]:
+        expenses_sell = int(i_item[2])  # сразу добавляем стоимость покупки чертежа
+        expenses_buy = int(i_item[2])  # сразу добавляем стоимость покупки чертежа
+
+        for i, i_material in enumerate(items_data[0][5:]):
+            material_price_sell = int(i_item[5+i]) * float(prices[i_material]['sell'])
+            expenses_sell += material_price_sell
+
+            material_price_buy = int(i_item[5+i]) * float(prices[i_material]['buy'])
+            expenses_buy += material_price_buy
+
+        i_profit_sell = int(prices[i_item[0]]['sell']) - expenses_sell
+        i_profit_buy = int(prices[i_item[0]]['sell']) - expenses_buy
+
+        k_sell = round(i_profit_sell/int(i_item[1])/1000, 2)
+        k_buy = round(i_profit_buy/int(i_item[1])/1000, 2)
+
+        result.append([i_item[0], i_item[1], expenses_sell, expenses_buy, i_profit_sell, i_profit_buy, k_sell, k_buy])
+
+    return result
+
+
+def results_to_file(results, hub):
+    with open(f"result_{hub}.csv", "a", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(
+            ['item', 'lp', 'expenses_sell', 'expenses_buy', 'i_profit_sell', 'i_profit_buy', 'k_sell', 'k_buy']
+        )
+        for line in results:
+            writer.writerow(line)
+
+    print('File writed!')
+
+
+production_data = data_reader("data.csv")
+trade_hub = 'jita'
+price_data = price_parser(production_data, trade_hub)
+profit_dict = profit_calc(production_data, price_data)
+results_to_file(profit_dict, trade_hub)
